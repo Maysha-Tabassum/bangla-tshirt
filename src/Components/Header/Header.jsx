22e562f75140e9526362cc0faef2573f32aa6ccf
Maysha-Tabassum <mayshatabassum77@gmail.com>
2023-11-22 15:31:15 -0800

    initial data load

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='navlinks'>
+            <Link to="/">Home</Link>
+            <Link to="/orders">Orders Review</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/contact">Contact Us</Link>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/Layout/Main';
+import Home from './Components/Home/Home';
+import OrderReview from './Components/OrderReview/OrderReview';
+import Aboutus from './Components/Aboutus/Aboutus';
+import Contact from './Components/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: "orders",
+        element: <OrderReview></OrderReview>
+      },
+      {
+        path: "about",
+        element: <Aboutus></Aboutus>
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
